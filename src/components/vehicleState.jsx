c7e3ece08a71885f43305b5cab7939e0862590f0
Tharindu N. Madhusanka <tharindu.nm@yahoo.com>
2023-06-14 00:03:20 +0530

    vehicleState page update

--- src/components/vehicleState.jsx
+++ src/components/vehicleState.jsx
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
+import "./styles/vehicleState.css";
+
 const VehicleState = () => {
 	const [state, setState] = useState("Loading...");
 	const [speed, setSpeed] = useState(0);
 	const [responseData, setResponseData] = useState(null);
+	const [uniqueStates, setUniqueStates] = useState([]);
 	const [lastUpdate, setLastUpdate] = useState(null);
 
 	useEffect(() => {
@@ -13,8 +16,35 @@ const VehicleState = () => {
 				const response = await axios.get("https://api.driversafe.tharindu.dev/states");
 				setResponseData(response.data);
 
+				let uniqueStates = [];
+				let previousState = response.data[0];
+				response.data.forEach((data) => {
+					if (data.state !== previousState.state) {
+						previousState = data;
+						uniqueStates.push(data);
+					}
+				});
+
+				setUniqueStates(uniqueStates);
+
 				setLastUpdate(response.data[0].time);
-				setState(response.data[0].state);
+
+				if (response.data[0].state === "Driving") {
+					setState("Driving 🚗");
+				} else if (response.data[0].state === "Breaked") {
+					setState("Breaked 🛑");
+				} else if (response.data[0].state === "Parking") {
+					setState("Parked 🅿️");
+				} else if (response.data[0].state === "Stopped") {
+					setState("Stopped 🅿️");
+				} else if (response.data[0].state === "Accidented") {
+					setState("Accidented 💥");
+				} else if (response.data[0].state === "Unknown") {
+					setState("Unknown 🤷‍♂️");
+				} else {
+					setState("Unknown 🤷‍♂️");
+				}
+
 				setSpeed(response.data[0].speed);
 			} catch (error) {
 				console.error(error);
@@ -23,8 +53,7 @@ const VehicleState = () => {
 
 		fetchData();
 
-		const interval = setInterval(fetchData, 5000);
-
+		const interval = setInterval(fetchData, 2000);
 		return () => clearInterval(interval);
 	}, []);
 
@@ -40,10 +69,8 @@ const VehicleState = () => {
 
 		const timeDifference1 = currentTime - time1;
 
-		if (timeDifference1 < 6000) {
-			setState("Driving 🚘");
-		} else {
-			setState("Stopped 🅿️");
+		if (timeDifference1 > 6000) {
+			setState("Unknown 🤷‍♂️");
 		}
 
 		const utcTime = new Date(lastUpdate);
@@ -53,10 +80,47 @@ const VehicleState = () => {
 
 	return (
 		<React.Fragment>
-			<h1>Status 🚗</h1>
-			<p>Last Update: {lastUpdate}</p>
-			<p>State: {state}</p>
-			<p>Speed: {speed}</p>
+			<div className="vehicle-state-container">
+				<div className="vehicle-state">
+					<h1>Status 🚗</h1>
+					<p>Last Update: {lastUpdate}</p>
+					<p>State: {state}</p>
+					<p>Speed: {speed}</p>
+				</div>
+
+				<div className="vehicle-state-table">
+					<table className="table table-sm table-light">
+						<thead>
+							<tr>
+								<th scope="col">#</th>
+								<th scope="col">State</th>
+								<th scope="col">Speed</th>
+								<th scope="col">Time</th>
+							</tr>
+						</thead>
+						<tbody className="table-body">
+							{uniqueStates &&
+								uniqueStates.slice(0, 50).map((data, index) => (
+									<tr
+										className={
+											data.state === "Accidented"
+												? "table-danger"
+												: data.state === "Breaked"
+												? "table-warning"
+												: ""
+										}
+										key={index}
+									>
+										<th scope="row">{index + 1}</th>
+										<td>{data.state}</td>
+										<td>{data.speed}</td>
+										<td>{data.time}</td>
+									</tr>
+								))}
+						</tbody>
+					</table>
+				</div>
+			</div>
 		</React.Fragment>
 	);
 };
